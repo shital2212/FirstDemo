0d7005d2bc3f77a1ea880c975bea0e435d0f7d3c
shital2212 <techie.shital@gmail.com>
2026-01-13 13:49:22 +0530

    Create HelloWorld.java
    new java program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+public static void main(String args[]){
+System.out.println("Hello Working with Core Java Programming!!");
+System.out.println("Test the code....");
+System.out.println("Changes done to code on 16th Dec'25....");
+}
+}
